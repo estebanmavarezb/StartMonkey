7362e94290bfa757a48f57f3e080bcb41813e44e
Ruben Mavarez <rubenmavarezb@gmail.com>
2020-11-05 23:04:24 -0300

    Adding functionality to burger menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log('Test')
+//Object that contains the strings of the HTML
+const dom = {
+    bars: '.bars',
+    navList: '.nav-list'
+}
+
+//Function that returns a querySelector
+const sE = elem => document.querySelector(elem);
+
+window.onload = () => {
+
+    //Event to open the menu
+    sE(dom.bars).addEventListener('click', () => {
+        sE(dom.navList).classList.toggle('nav-list_active')
+    })
+
+}
