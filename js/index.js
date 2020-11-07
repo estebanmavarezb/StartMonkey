4338d04609cdbeb382098accb644feedda8ec817
Ruben Mavarez <rubenmavarezb@gmail.com>
2020-11-07 08:51:36 -0300

    Adding close button and fuctionality to it

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 //Object that contains the strings of the HTML
 const dom = {
     bars: '.bars',
-    navList: '.nav-list'
+    navList: '.nav-list',
+    menuClose: '.bars.close'
 }
 
 //Function that returns a querySelector
@@ -9,9 +10,22 @@ const sE = elem => document.querySelector(elem);
 
 window.onload = () => {
 
+    const bars = sE(dom.bars);
+    const navList = sE(dom.navList);
+    const menuClose = sE(dom.menuClose);
+
     //Event to open the menu
-    sE(dom.bars).addEventListener('click', () => {
-        sE(dom.navList).classList.toggle('nav-list_active')
-    })
+    bars.onclick = () => {
+        navList.classList.toggle('nav-list_active');
+        bars.style.display = 'none';
+        menuClose.style.display ='block';
+    }
+
+    //Event to close the menu
+    menuClose.onclick = () => {
+        navList.classList.toggle('nav-list_active');
+        menuClose.style.display = 'none';
+        bars.style.display ='block';
+    }
 
 }
